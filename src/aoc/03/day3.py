36c9d1b9b52703f91ca86fc7322ddb545660448f
Marvin Schenkel <marvinschenkel@gmail.com>
2022-12-03 14:13:50 +0100

    Using set.intersection

--- src/aoc/03/day3.py
+++ src/aoc/03/day3.py
@@ -1,14 +1,14 @@
+from code import interact
 from functools import reduce
 
 def read_input(filename: str):
     with open(filename, "r") as f:
         return [x.rstrip() for x in f.readlines()]
-    
+
+
 def get_priority(character: str):
     return ord(character) - 96 if character.islower() else ord(character) - 38
 
-def get_intersect(set_a: list, set_b: list):
-    return set(set_a) & set(set_b)
 
 def one():
     priorities = []
@@ -16,15 +16,16 @@ def one():
         half = len(rucksack) // 2
         comp1 = rucksack[:half]
         comp2 = rucksack[half:]
-        double_item = list(get_intersect(comp1, comp2))[0]
+        double_item = list(set(comp1).intersection(set(comp2)))[0]
         priorities.append(get_priority(double_item))
     print(f"[1]: {sum(priorities)}")
+
 
 def two():
     rucksacks = read_input("src/aoc/03/day3.txt")
     groups = zip(*[rucksacks[i::3] for i in range(3)])
     common_items = [
-        list(reduce(get_intersect, group_rucksacks))[0]
+        list(reduce(set.intersection, [set(r) for r in group_rucksacks]))[0]
         for group_rucksacks in groups
     ]
     priorities = [get_priority(i) for i in common_items]
