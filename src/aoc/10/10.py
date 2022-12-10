419002db47555c554edda86406a55faf5c4290f8
Marvin Schenkel <marvinschenkel@gmail.com>
2022-12-10 14:13:54 +0100

    Refactory day 10

--- src/aoc/10/10.py
+++ src/aoc/10/10.py
@@ -12,39 +12,27 @@ def get_instructions():
     return list(line.split(" ") for line in read_input("src/aoc/10/10.txt"))
 
 
-def one():
+def run():
     instructions = get_instructions()
     X = 1
     cycles = 0
     signal_strenghts = []
-    for ins in instructions:
-        for tick in range(2) if ins[0] == "addx" else range(1):
-            cycles += 1
-            signal_strenghts.append(cycles * X if cycles % 40 == 20 else 0)
-            if tick == 1:
-                X += int(ins[1])
-    print(f"[1]: {sum(signal_strenghts)}")
-
-
-def two():
-    instructions = get_instructions()
-    X = 1
-    cycles = 0
     CRT = []
     for ins in instructions:
         for tick in range(2) if ins[0] == "addx" else range(1):
             cycles += 1
+            signal_strenghts.append(cycles * X if cycles % 40 == 20 else 0)
             CRT += "#" if (cycles - 1) % 40 in (X-1, X, X+1) else "."
             if tick == 1:
                 X += int(ins[1])
+    print(f"[1]: {sum(signal_strenghts)}")
     print("[2]:")
     for idx in range(0, 240, 40):
         print("".join(CRT[idx:idx+40]))
 
 
 def main():
-    one() 
-    two()
+    run()
 
 
 if __name__ == "__main__":
