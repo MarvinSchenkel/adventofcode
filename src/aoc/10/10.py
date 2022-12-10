d64f1682cb6de4f35ef6a349d9d0f775162cfe40
Marvin Schenkel <marvinschenkel@gmail.com>
2022-12-10 14:08:52 +0100

    Refactory day 10

--- src/aoc/10/10.py
+++ src/aoc/10/10.py
@@ -7,57 +7,43 @@ def read_input(filename: str):
     with open(filename, "r") as f:
         return [x.rstrip() for x in f.readlines()]
 
-def parse_instructions(instructions: list[str]):
-    return list(line.split(" ") for line in read_input())
 
-
-def check_signal_strength(cycle, X):
-    return cycle * X if cycle in [20, 60, 100, 140, 180, 220] else 0
+def get_instructions():
+    return list(line.split(" ") for line in read_input("src/aoc/10/10.txt"))
 
 
 def one():
-    instructions = list(line.split(" ") for line in read_input("src/aoc/10/10.txt"))
+    instructions = get_instructions()
     X = 1
     cycles = 0
     signal_strenghts = []
     for ins in instructions:
-        if ins[0] == "noop":
+        for tick in range(2) if ins[0] == "addx" else range(1):
             cycles += 1
-            signal_strenghts.append(check_signal_strength(cycles, X))
-        elif ins[0] == "addx":
-            cycles += 1
-            signal_strenghts.append(check_signal_strength(cycles, X))
-            cycles += 1
-            signal_strenghts.append(check_signal_strength(cycles, X))
-            X += int(ins[1])
+            signal_strenghts.append(cycles * X if cycles in [20, 60, 100, 140, 180, 220] else 0)
+            if tick == 1:
+                X += int(ins[1])
     print(f"[1]: {sum(signal_strenghts)}")
-    
-
-def draw(pos, X):
-    return "#" if pos % 40 in [X-1, X, X+1] else "."
 
 
 def two():
-    instructions = list(line.split(" ") for line in read_input("src/aoc/10/10.txt"))
+    instructions = get_instructions()
     X = 1
     cycles = 0
     CRT = []
     for ins in instructions:
-        if ins[0] == "noop":
+        for tick in range(2) if ins[0] == "addx" else range(1):
             cycles += 1
-            CRT += draw(cycles - 1, X)
-        elif ins[0] == "addx":
-            cycles += 1
-            CRT += draw(cycles - 1, X)
-            cycles += 1
-            CRT += draw(cycles - 1, X)
-            X += int(ins[1])
+            CRT += "#" if (cycles - 1) % 40 in [X-1, X, X+1] else "."
+            if tick == 1:
+                X += int(ins[1])
     print("[2]:")
     for idx in range(0, 240, 40):
         print("".join(CRT[idx:idx+40]))
 
+
 def main():
-    one()
+    one() 
     two()
 
 
