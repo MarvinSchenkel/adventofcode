8afab76af7b10a7fac77189d31cb6310aa6a2302
Marvin Schenkel <marvinschenkel@gmail.com>
2022-12-15 17:52:54 +0100

    Optimize 14

--- src/aoc/14/14.py
+++ src/aoc/14/14.py
@@ -24,62 +24,64 @@ def one(rocks):
     min_x = min(r[0] for r in rocks)
     max_y = max(r[1] for r in rocks)
     done = False
+    fallen_sand = 0
     while not done:
         s = (500, 0)
         while True:
             # Check if we hit the abyss
             if s[0] < min_x or s[1] + 1 > max_y:
-                print(f"[1]: {len(fallen_sand)}")
+                print(f"[1]: {fallen_sand}")
                 done = True
                 break
             # Keep falling down
-            if (s[0], s[1] + 1) not in (rocks | fallen_sand):
+            if (s[0], s[1] + 1) not in rocks:
                 s = (s[0], s[1] + 1)
                 continue
             # Check if we can go left
-            if (s[0] - 1, s[1] + 1) not in (rocks | fallen_sand):
+            if (s[0] - 1, s[1] + 1) not in rocks:
                 s = (s[0] - 1, s[1])
                 continue
             # Check if we can go right
-            if (s[0] + 1, s[1] + 1) not in (rocks | fallen_sand):
+            if (s[0] + 1, s[1] + 1) not in rocks:
                 s = (s[0] + 1, s[1])
                 continue
             # Place sand
-            fallen_sand.add(s)
+            rocks.add(s)
+            fallen_sand += 1
             break
 
 
 def two(rocks):
-    fallen_sand = set()
+    fallen_sand = 0
     max_y = max(r[1] for r in rocks)
     floor = max_y + 2
     # Add the floor
-    while (500, 0) not in fallen_sand:
+    while (500, 0) not in rocks:
         s = (500, 0)
         while True:
             # Keep falling down
-            if (s[0], s[1] + 1) not in (rocks | fallen_sand) and s[1] + 1 != floor:
+            if (s[0], s[1] + 1) not in rocks and s[1] + 1 != floor:
                 s = (s[0], s[1] + 1)
                 continue
             # Check if we can go left
-            if (s[0] - 1, s[1] + 1) not in (rocks | fallen_sand) and s[1] + 1 != floor:
+            if (s[0] - 1, s[1] + 1) not in rocks and s[1] + 1 != floor:
                 s = (s[0] - 1, s[1])
                 continue
             # Check if we can go right
-            if (s[0] + 1, s[1] + 1) not in (rocks | fallen_sand) and s[1] + 1 != floor:
+            if (s[0] + 1, s[1] + 1) not in rocks and s[1] + 1 != floor:
                 s = (s[0] + 1, s[1])
                 continue
             # Place sand
-            fallen_sand.add(s)
+            rocks.add(s)
+            fallen_sand += 1
             break
-        print(f"Sand count: {len(fallen_sand)}")
-    print(f"[2]: {len(fallen_sand)}")
+    print(f"[2]: {fallen_sand}")
 
 
 def run():
     lines = read_input("src/aoc/14/14.txt")
     rocks = parse_cave(lines)
-    #one(rocks)
+    one(rocks.copy())
     two(rocks)
 
 
